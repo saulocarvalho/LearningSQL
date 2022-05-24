a67a80df0301524c735a8658a007b3da958160db
Saulo Dias de Carvalho <Saulo.Dias@ESPACOEDUCAR.INT>
2022-05-24 17:10:01 -0300

    first commit

---
+++ first.sql
@@ -0,0 +1,24 @@
+CREATE TABLE pessoas (
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    nascimento DATE
+)
+
+
+INSERT INTO pessoa (nome, nascimento) VALUES ('Saulo', '2000-03-01')
+INSERT INTO pessoa (nome, nascimento) VALUES ('Dias', '2001-08-22')
+INSERT INTO pessoa (nome, nascimento)) VALUES ('Carvalho', '2003-12-25')
+
+
+
+
+UPDATE pessoa SET nome = 'Saulo Carvalho' WHERE id=1;
+
+
+DELETE FROM pessoa WHERE id=1;
+
+
+SELECT * FROM pessoa WHERE id=1; //checar informação
+
+SELECT * FROM pessoa ORDER BY nome //Ordenação por ordem crescente, nome
+SELECT * FROM pessoa ORDER BY nome DESC //Ordenação por ordem descrente, nome
